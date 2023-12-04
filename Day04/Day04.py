1355aa335820783838433d7d7f788ae94344f9d4
Jorge Saura <jorge.saura@mdwpartners.com>
2023-12-04 21:49:40 +0100

    Advent of Code Day 04. Second part solved

--- Day04/Day04.py
+++ Day04/Day04.py
@@ -33,4 +33,30 @@ class Scratchcards:
             cards_points = (cards_points + pow(2,total_winning_numbers-1)) if total_winning_numbers != 0 else cards_points
             
         return int(cards_points)
+    
+    def _add_copy_cards(self, cards:dict, starting_card: int, next_cards_win:int, num_copies_starting_card: int)-> dict:
+
+        for i in range(starting_card , starting_card + next_cards_win):
+            cards[i]= cards.get(i,0) + (1 * num_copies_starting_card)
+          
+        
+        return cards
+
+
+    def get_cards_points_2(self, path:str)-> int:
+
+        lines = self._read_data(path)
+        cards  = dict()
+
+        for pos, line in enumerate(lines,1):
+            cards[pos] = cards.get(pos,0) + 1 # Actual card
+            numbers_in_line = self._get_numbers_in_line(line)
+            next_cards_to_add = len([n for n,v in numbers_in_line.items() if v > 1]) 
+            cards = self._add_copy_cards(cards, pos + 1, next_cards_to_add, cards[pos])
+
+
+        total_cards = sum((value for key,value in cards.items())) 
+            
+        return total_cards
+
 
--- Day04/test_Day04.py
+++ Day04/test_Day04.py
@@ -6,7 +6,7 @@ import Day04.Day04 as challenges
 
 class TestDay04(unittest.TestCase):
 
-    def test_GearRatios(self):
+    def test_Scratchcards(self):
 
         cards = challenges.Scratchcards()
 
@@ -23,7 +23,27 @@ class TestDay04(unittest.TestCase):
         self.assertEqual(winnig_points, 23028)
 
 
+    def test_Scratchcards_2(self):
 
+        cards = challenges.Scratchcards()
+
+        d = {0:2,1:1,5:7}
+        d = cards._add_copy_cards(d,starting_card=1,next_cards_win=5,num_copies_starting_card=1)
+        self.assertEqual(d[1], 2)
+        self.assertEqual(d[5], 8)
+
+        d = {0:2,1:1,5:7}
+        d = cards._add_copy_cards(d,starting_card=1,next_cards_win=5,num_copies_starting_card=2)
+        self.assertEqual(d[1], 3)
+        self.assertEqual(d[5], 9)
+
+
+        winnig_points = cards.get_cards_points_2("Day04\DataSimple.txt")
+        self.assertEqual(winnig_points, 30)
+
+        winnig_points = cards.get_cards_points_2("Day04\Data.txt")
+        self.assertEqual(winnig_points, 9236992)
+        
 
 if __name__ == "__main__":
 
