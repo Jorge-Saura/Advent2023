5deafac9293823921c89356d851569f9715db85e
Jorge Saura <jorge.saura@live.com>
2023-12-08 22:01:53 +0100

    Advent of code Day 05: Second part completed

--- Day05/Day05.py
+++ Day05/Day05.py
@@ -43,7 +43,66 @@ class Agriculture:
 
             
         return min(locations)
-    
 
+
+    def _compose_range(self, rng1: tuple, rng2: tuple) -> list:
+        composed_range = list()
+        c_src, c_offset = rng1    
+        dst, src, offset = rng2
+
+        if (c_src + c_offset - 1) < src: # fuera del intervalo por la izquierda
+            composed_range.append((c_src, c_offset,'original')) 
+        if c_src < src and src <= (c_src + c_offset - 1) <= (src + offset - 1): # fuera por la izquierda dentro por la derecha
+            composed_range.append((c_src, (src -c_src),'original'))
+            composed_range.append((dst, c_offset - (src - c_src),'transformed'))
+        if  src <= c_src and (c_src + c_offset) <= (src + offset) : # dentro la izquierda dentro por la derecha
+            composed_range.append((dst + (c_src - src), c_offset,'transformed'))
+        if  src <= c_src <= src + (offset -1) and (c_src + c_offset) > (src + offset) : # dentro la izquierda fuera por la derecha
+            composed_range.append((dst + (c_src - src), (src + offset) - c_src,'transformed'))
+            composed_range.append((( src + offset ),  (c_src + c_offset) - (src + offset),'original'))
+        if c_src > src + (offset - 1): # fuera del intervalo por la derecha
+            composed_range.append((c_src, c_offset,'original')) 
+        if c_src < src and src + offset < c_src + c_offset: #fuera por la izqd y fuera por la derecha (el intervalo a comprobar está dentro del intervalo actual)
+            composed_range.append((c_src, (src -c_src),'original'))
+            composed_range.append((dst, offset,'transformed'))
+            composed_range.append((src + offset, (c_src + c_offset) - (src + offset),'original'))
+
+        return composed_range
+
+
+    def get_min_location_2(self, path:str)-> int:
+
+        self._load_data(path)
+        
+        locations = list()
+
+        seeds_ranges =[(self.seeds[i], self.seeds[i+1]) for i in range(0, len(self.seeds)-1, 2)]
+        final_ranges = list()
+
+        for seed_range  in seeds_ranges:
+            original_ranges = [seed_range]
+            new_ranges = list()
+            checked_ranges = list()
+
+            for step in range(0,7):
+                for check_range in self.cultivation[step]:
+                    while original_ranges:
+                        current = original_ranges.pop(0)
+                        
+                        composed_ranges = self._compose_range(current,tuple(check_range))
+                        checked_ranges.extend([(n[0],n[1]) for n in composed_ranges if n[2] == 'original'])
+                        new_ranges.extend([(n[0],n[1]) for n in composed_ranges if n[2] == 'transformed'])
+                
+                    original_ranges.extend(checked_ranges)
+                    checked_ranges = list()
+                
+                original_ranges.extend(new_ranges)
+                checked_ranges = list()
+                new_ranges = list()
+            
+            final_ranges.extend(original_ranges)
+
+            
+        return min(final_ranges,key=lambda x: x[0])[0]
 
 
--- Day05/test_Day05.py
+++ Day05/test_Day05.py
@@ -6,7 +6,7 @@ import Day05.Day05 as challenges
 
 class TestDay04(unittest.TestCase):
 
-    def test_Scratchcards(self):
+    def test_Cultivation(self):
 
         cultivation = challenges.Agriculture()
 
@@ -18,9 +18,92 @@ class TestDay04(unittest.TestCase):
         
 
         min_location = cultivation.get_min_location("Day05\Data.txt")
-        self.assertEqual(min_location, 35)
+        self.assertEqual(min_location, 836040384)
+    
+
+    def test__compose_range(self):
+
+        cultivation = challenges.Agriculture()
+
+        #Fuera por la izqd 
+        composed = cultivation._compose_range((10,3),(0,20,10))
+        self.assertEqual(composed[0], (10,3,'original'))
+
+        composed = cultivation._compose_range((10,10),(0,20,10))
+        self.assertEqual(composed[0], (10,10,'original'))
+
+        # Fuera por la izqd dentro por la derecha
+        composed = cultivation._compose_range((10,10),(0,15,20))
+        self.assertEqual(composed[0], (10,5,'original'))
+        self.assertEqual(composed[1], (0,5,'transformed'))
+
+        composed = cultivation._compose_range((10,6),(0,15,20))
+        self.assertEqual(composed[0], (10,5,'original'))
+        self.assertEqual(composed[1], (0,1,'transformed'))
+
+        # Dentro del rango
+        composed = cultivation._compose_range((10,5),(0,5,25))
+        self.assertEqual(composed[0], (5,5,'transformed'))
+
+        composed = cultivation._compose_range((10,5),(0,10,5))
+        self.assertEqual(composed[0], (0,5,'transformed'))
+
+        composed = cultivation._compose_range((10,3),(0,10,5))
+        self.assertEqual(composed[0], (0,3,'transformed'))
+
+        composed = cultivation._compose_range((12,3),(0,10,5))
+        self.assertEqual(composed[0], (2,3,'transformed'))
+
+        composed = cultivation._compose_range((10,1),(0,10,5))
+        self.assertEqual(composed[0], (0,1,'transformed'))
+
+        composed = cultivation._compose_range((14,1),(0,10,5))
+        self.assertEqual(composed[0], (4,1,'transformed'))
+
+        #Dentro por la izqd fuera por la drch
+        composed = cultivation._compose_range((15,10),(0,10,10))
+        self.assertEqual(composed[0], (5,5,'transformed'))
+        self.assertEqual(composed[1], (20,5,'original'))
+        
+        composed = cultivation._compose_range((19,2),(0,10,10))
+        self.assertEqual(composed[0], (9,1,'transformed'))
+        self.assertEqual(composed[1], (20,1,'original'))
+
+        composed = cultivation._compose_range((57,13),(49,53,8))
+        self.assertEqual(composed[0], (53,4,'transformed'))
+        self.assertEqual(composed[1], (61,9,'original'))
+
+        #Fuera por la drcha
+        composed = cultivation._compose_range((25,5),(0,10,10))
+        self.assertEqual(composed[0], (25,5,'original'))
+
+        composed = cultivation._compose_range((20,5),(0,10,10))
+        self.assertEqual(composed[0], (20,5,'original'))
+
+        #Fuera por la izqd y drcha (es más grande que el rango a comprobar, lo contiene)
+        composed = cultivation._compose_range((5,20),(0,10,10))
+        self.assertEqual(composed[0], (5,5,'original'))
+        self.assertEqual(composed[1], (0,10,'transformed'))
+        self.assertEqual(composed[2], (20,5,'original'))
+
+        composed = cultivation._compose_range((9,12),(0,10,10))
+        self.assertEqual(composed[0], (9,1,'original'))
+        self.assertEqual(composed[1], (0,10,'transformed'))
+        self.assertEqual(composed[2], (20,1,'original'))
 
 
+
+
+
+    def test_Cultivatio_2n(self):
+
+        cultivation = challenges.Agriculture()
+
+        min_location = cultivation.get_min_location_2("Day05\DataSimple.txt")
+        self.assertEqual(min_location, 46)
+
+        min_location = cultivation.get_min_location_2("Day05\Data.txt")
+        self.assertEqual(min_location, 10834440)
 
 
 
