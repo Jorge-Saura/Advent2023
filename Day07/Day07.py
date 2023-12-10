8c4a8c14b34115353f3edfcfdc00fdf10be1be36
jorge-saura <jorge.saura@live.com>
2023-12-10 17:44:57 +0100

    Advent of code Day 07: First part completed

--- Day07/Day07.py
+++ Day07/Day07.py
@@ -1,4 +1,5 @@
 from functools import reduce
+from collections import Counter
 import re
 
 class CamelCards:
@@ -8,14 +9,86 @@ class CamelCards:
         hands = list()
         with open(path, "r") as file:
             # Reading from a file
+            for line in file:
+                cards, bid = line.replace('\n','').split(' ')
 
-            line = file.readline()
-            cards, bid = line.replace('\n','').split(' ')
-            hands.append((cards,int(bid)))
+                tranlated_cards = cards.translate(str.maketrans({'A': 'Z'
+                                                       , 'K': 'Y'
+                                                       , 'Q': 'X'
+                                                       , 'J': 'W'
+                                                       , 'T': 'V'
+                                                       , '9': 'U'
+                                                       , '8': 'T'
+                                                       , '7': 'S'
+                                                       , '6': 'R'
+                                                       , '5': 'Q'
+                                                       , '4': 'P'
+                                                       , '3': 'O'
+                                                       , '2': 'N'}))
+                hands.append((tranlated_cards,int(bid), cards))
 
   
         return hands
     
+    def _get_hand_type(self, cards:str) -> str:
+        hand_type = ''
+
+        # types are: five, four, full, three, two, one, high
+
+        numbers = Counter(cards)
+        numbers = sorted(numbers.items(),key=lambda item: item[1],reverse=True)
+        if numbers[0][1] == 5:
+            return 'five'
+        if numbers[0][1] == 4:
+            return 'four'
+        if numbers[0][1] == 3 and numbers[1][1] == 2:
+            return 'full'
+        if numbers[0][1] == 3 and numbers[1][1] == 1:
+            return 'three'
+        if numbers[0][1] == 2 and numbers[1][1] == 2:
+            return 'two'
+        if numbers[0][1] == 2 and numbers[1][1] == 1:
+            return 'one'
+        if numbers[0][1] == 1:
+            return 'high'
+
+        return 'otro'
+    
+    def get_total_winnings(self, path:str)-> int:
+
+        hands = self._load_data(path)
+        total_winnings = 0
+        sorted_bids = list()
+        hand_types = dict()
+
+        
+        for cards,bid, original in hands:
+            hand_type = self._get_hand_type(cards)
+            current_list = hand_types.get(hand_type, list())
+            current_list.append((cards,bid, original))
+            hand_types[hand_type] = current_list
+        
+
+        
+        bids = hand_types.get('high',list())
+        sorted_bids.extend(sorted(bids,key=lambda x: x[0]))
+        bids = hand_types.get('one',list())
+        sorted_bids.extend(sorted(bids,key=lambda x: x[0]))
+        bids = hand_types.get('two',list())
+        sorted_bids.extend(sorted(bids,key=lambda x: x[0]))
+        bids = hand_types.get('three',list())
+        sorted_bids.extend(sorted(bids,key=lambda x: x[0]))
+        bids = hand_types.get('full',list())
+        sorted_bids.extend(sorted(bids,key=lambda x: x[0]))
+        bids = hand_types.get('four',list())
+        sorted_bids.extend(sorted(bids,key=lambda x: x[0]))
+        bids = hand_types.get('five',list())
+        sorted_bids.extend(sorted(bids,key=lambda x: x[0]))
+        
+        for pos, bid in enumerate(sorted_bids,1):
+            total_winnings +=  pos * bid[1]
+
+        return total_winnings
 
 
 
--- Day07/test_Day07.py
+++ Day07/test_Day07.py
@@ -3,6 +3,8 @@
 import unittest
 
 import Day07.Day07 as challenges
+from itertools import permutations
+
 
 class TestDay07(unittest.TestCase):
 
@@ -12,10 +14,64 @@ class TestDay07(unittest.TestCase):
 
         hands = cards._load_data("Day07\DataSimple.txt")
         self.assertEqual(len(hands),5)
-        self.assertEqual(hands[0][0],'32T3K')
+        self.assertEqual(hands[0][0],'ONVOY')
         self.assertEqual(hands[0][1],765)
- 
+        self.assertEqual(hands[1][0],'VQQWQ')
+        self.assertEqual(hands[1][1],684)
 
+    def test_get_hand_type(self):
+
+        cards = challenges.CamelCards()
+
+        hand = cards._get_hand_type('KKKKK')
+        self.assertEqual(hand,'five')
+
+        p = {"".join(p) for p in permutations('KK4KK')}
+
+        for value in p:
+            hand = cards._get_hand_type(value)
+            self.assertEqual(hand,'four')
+ 
+        p = {"".join(p) for p in permutations('KKK11')}
+
+        for value in p:
+            hand = cards._get_hand_type(value)
+            self.assertEqual(hand,'full')
+
+        p = {"".join(p) for p in permutations('KKK31')}
+
+        for value in p:
+            hand = cards._get_hand_type(value)
+            self.assertEqual(hand,'three')
+
+        p = {"".join(p) for p in permutations('KK233')}
+
+        for value in p:
+            hand = cards._get_hand_type(value)
+            self.assertEqual(hand,'two')
+
+        p = {"".join(p) for p in permutations('KK234')}
+
+        for value in p:
+            hand = cards._get_hand_type(value)
+            self.assertEqual(hand,'one')
+
+        p = {"".join(p) for p in permutations('K2345')}
+
+        for value in p:
+            hand = cards._get_hand_type(value)
+            self.assertEqual(hand,'high')
+
+    def test_get_total_winnings(self):
+
+        cards = challenges.CamelCards()
+
+        total = cards.get_total_winnings("Day07\DataSimple.txt")
+        self.assertEqual(total,6440)
+
+        # 209380301 too low
+        total = cards.get_total_winnings("Day07\Data.txt")
+        self.assertEqual(total,249638405)
 
 if __name__ == "__main__":
 
