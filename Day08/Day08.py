a8ea9f32fbe5ec1df7c7a2f99f728c5d120b4de0
jorge-saura <jorge.saura@live.com>
2023-12-11 23:52:49 +0100

    Advent of code Day 08: Second part completed

--- Day08/Day08.py
+++ Day08/Day08.py
@@ -1,6 +1,4 @@
-from functools import reduce
-from collections import Counter
-import re
+import math
 
 class NetworkNavigator:
 
@@ -40,6 +38,52 @@ class NetworkNavigator:
 
  
         return number_of_steps
+    
+    def _get_nodes_ending_with_char(self, nodes:list, ending_char:str) -> list():
+        return [n for n in nodes if n[-1] == ending_char]
+
+
+    def _get_number_of_steps(self, steps:str, nodes:dict, start_node:str, end_node:str) -> int:
+        number_of_steps = 0
+        current_node = start_node
+        total_steps = len(steps)
+
+        while current_node != end_node:
+            current_step = steps[number_of_steps%total_steps]
+            current_node = nodes[current_node][0] if current_step == 'L' else nodes[current_node][1]
+            number_of_steps += 1
+        
+        return number_of_steps
+
+    def get_number_of_steps_multiple_starts(self, path:str)-> int:
+
+        steps, nodes = self._load_data(path)
+
+        number_of_steps = 0
+        starting_nodes = self._get_nodes_ending_with_char(nodes.keys(),'A')
+        
+        
+        node_costs = dict()
+        node_cicles = {n:0 for n in starting_nodes}
+        node_step = {n:n for n in starting_nodes}
+        node_endings = {n:list() for n in starting_nodes}
+        total_steps = len(steps)
+
+        while not all(n for n in node_cicles.values()): #all starting nodes have a cicle
+            for start_node in starting_nodes:
+                current_step = steps[number_of_steps%total_steps]
+                
+                
+                node_step[start_node] = nodes[node_step[start_node]][0] if current_step == 'L' else nodes[node_step[start_node]][1]
+                if node_step[start_node][-1] == 'Z':
+                    node_cicles[start_node] = node_cicles[start_node] + 1
+                    node_endings[start_node].append(number_of_steps + 1)
+                
+            number_of_steps += 1
+        
+        lcm_value = math.lcm(*[n[0] for n in node_endings.values()])
+ 
+        return lcm_value
 
 
 
--- Day08/test_Day08.py
+++ Day08/test_Day08.py
@@ -21,8 +21,22 @@ class TestDay08(unittest.TestCase):
         self.assertEqual(steps,6)
 
         steps = navigator.get_number_of_steps("Day08\Data.txt")
-        self.assertEqual(steps,6)
-    
+        self.assertEqual(steps,19637)
+
+    def test_nodes_ending_with(self):
+
+        navigator = challenges.NetworkNavigator()
+        data = navigator._get_nodes_ending_with_char(['AB','BC','ZZ','EE','AZ'], 'Z')
+        self.assertEqual(data,['ZZ','AZ'])    
+
+    def test_multiple_starts(self):
+
+        navigator = challenges.NetworkNavigator()
+        data = navigator.get_number_of_steps_multiple_starts("Day08\DataSimple3.txt")
+        self.assertEqual(data,6)
+
+        data = navigator.get_number_of_steps_multiple_starts("Day08\Data.txt")
+        self.assertEqual(data,8811050362409)
 
 if __name__ == "__main__":
 
